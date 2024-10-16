55317cea2132acc844d500fb550f7ce3f114acca
Gayana Senarathne <gayana.senarathne@cammsgroup.com>
2024-10-16 13:22:15 +0530

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:travelapp/app/app.dart';
+import 'package:travelapp/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const App());
 }
