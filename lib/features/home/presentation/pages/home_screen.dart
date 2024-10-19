38305d8ab390fe776ce00ba9bd71948997703fc8
Gayana Senarathne <gayana.senarathne@cammsgroup.com>
2024-10-20 01:44:51 +0530

    Upcoming events and populer destinations data functions implementation.

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -10,7 +10,9 @@ import 'package:travelapp/core/resources/text_styles.dart';
 import 'package:travelapp/features/common/cubit/connectivity_cubit/connectivity_cubit.dart';
 import 'package:travelapp/features/common/cubit/custom_loading/custom_loading_cubit.dart';
 import 'package:travelapp/features/common/widgets/custom_loading.dart';
+import 'package:travelapp/features/home/data/Services/firebase_services.dart';
 import 'package:travelapp/features/home/data/model/carousel_model.dart';
+import 'package:travelapp/features/home/data/model/detail_model.dart';
 import 'package:travelapp/features/home/presentation/cubit/cubit/page_indicator_cubit.dart';
 import 'package:travelapp/features/home/presentation/pages/user_profile_view.dart';
 import 'package:travelapp/features/home/presentation/widgets/carousel_card.dart';
@@ -31,63 +33,82 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late FirebaseServices firebaseServices;
   late FlutterSecureStorage secureStorage;
   late CustomLoadingCubit customLoadingCubit;
-  late String userImageUrl = 'https://picsum.photos/200/300'; //Note - This default value is a placeholder for image.
+  late String userImageUrl = '';
+
+  List<DetailModel> lisRatedDetailModel = [];
+  List<DetailModel> lisUpComingEvents = [];
 
   final CarouselSliderController _carouselController =
       CarouselSliderController();
 
-  List<CarouselModel> carouselItemsModel = [
-    CarouselModel(
-        title: "Item 1",
-        imageUrl: "https://picsum.photos/300/200",
-        locationTitle: "Kandy"),
-    CarouselModel(
-        title: "Item 2",
-        imageUrl: "https://picsum.photos/300/200",
-        locationTitle: "Kandy"),
-    CarouselModel(
-        title: "Item 3",
-        imageUrl: "https://picsum.photos/300/200",
-        locationTitle: "Kandy"),
-    CarouselModel(
-        title: "Item 4",
-        imageUrl: "https://picsum.photos/300/200",
-        locationTitle: "Kandy"),
-    CarouselModel(
-        title: "Item 5",
-        imageUrl: "https://picsum.photos/300/200",
-        locationTitle: "Kandy")
-  ];
+  @override
+  void initState() {
+    customLoadingCubit = CustomLoadingCubit();
+    secureStorage = const FlutterSecureStorage();
+    firebaseServices = FirebaseServices();
+    setUserImage();
+    getPopulerDestinationsListData();
+    getUpcomingEventsListData();
+    super.initState();
+  }
 
-  final List<CarouselModel> destinations = [
-    CarouselModel(
-        title: "Item 1",
-        imageUrl: "https://picsum.photos/300/200?random=1",
-        locationTitle: "Colombo",
-        rating: 4.0),
-    CarouselModel(
-        title: "Item 2",
-        imageUrl: "https://picsum.photos/300/200?random=2",
-        locationTitle: "Galle",
-        rating: 4.0),
-    CarouselModel(
-        title: "Item 3",
-        imageUrl: "https://picsum.photos/300/200?random=3",
-        locationTitle: "Jaffna",
-        rating: 4.0),
-    CarouselModel(
-        title: "Item 4",
-        imageUrl: "https://picsum.photos/300/300?random=4",
-        locationTitle: "Anuradhapura",
-        rating: 4.0),
-    CarouselModel(
-        title: "Item 5",
-        imageUrl: "https://picsum.photos/300/200?random=5",
-        locationTitle: "Kandy",
-        rating: 4.0),
-  ];
+  void setUserImage() async {
+    userImageUrl = await secureStorage.read(key: 'userImageUrl') ?? '';
+  }
+
+  void getPopulerDestinationsListData() async {
+    String collectionName = 'destinations';
+
+    List<DetailModel> lisDetailModel =
+        await firebaseServices.fetchAllData(collectionName: collectionName);
+
+    if (lisDetailModel.isNotEmpty) {
+      setState(() {
+        lisRatedDetailModel =
+            lisDetailModel.where((item) => item.rating >= 4.0).toList();
+      });
+    }
+  }
+
+  void getUpcomingEventsListData() async {
+    String collectionName = 'events';
+
+    // Fetch data from Firebase
+    List<DetailModel> lisDetailModel =
+        await firebaseServices.fetchAllData(collectionName: collectionName);
+
+    if (lisDetailModel.isNotEmpty) {
+      DateTime now = DateTime.now();
+      int currentMonth = now.month;
+
+      // Map of season months to their respective month numbers
+      Map<String, int> monthMap = {
+        'January': 1,
+        'February': 2,
+        'March': 3,
+        'April': 4,
+        'May': 5,
+        'June': 6,
+        'July': 7,
+        'August': 8,
+        'September': 9,
+        'October': 10,
+        'November': 11,
+        'December': 12,
+      };
+
+      // Filter events that are in the current month or in the future months
+      setState(() {
+        lisUpComingEvents = lisDetailModel.where((item) {
+          int eventMonth = monthMap[item.season] ?? 0;
+          return eventMonth >= currentMonth; // Filter future events
+        }).toList();
+      });
+    }
+  }
 
   void carouselOnTap() {
     if (kDebugMode) {
@@ -95,7 +116,7 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-    void setCurrentUserData() async {
+  void setCurrentUserData() async {
     final imageUrl = await secureStorage.read(key: 'userImageUrl') ?? '';
 
     // Update state only if necessary
@@ -105,16 +126,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  void initState() {
-    context.read<ConnectivityCubit>();
-    customLoadingCubit = CustomLoadingCubit();
-    secureStorage = const FlutterSecureStorage();
-    super.initState();
-  }
-
-  @override
   void dispose() {
-    // _carouselController.dispose();
     super.dispose();
   }
 
@@ -185,90 +197,108 @@ class _HomeScreenState extends State<HomeScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const TitleText(titleText: "Upcoming Events"),
-                  Padding(
-                    padding: const EdgeInsetsDirectional.only(
-                        top: Dimens.defaultPadding),
-                    child: CarouselSlider(
-                      carouselController: _carouselController,
-                      options: CarouselOptions(
-                        height: 200.0,
-                        autoPlay: true,
-                        autoPlayInterval: const Duration(seconds: 3),
-                        autoPlayAnimationDuration:
-                            const Duration(milliseconds: 800),
-                        autoPlayCurve: Curves.easeInOut,
-                        enlargeCenterPage: true,
-                        onPageChanged: (index, reason) {
-                          context
-                              .read<PageIndicatorCubit>()
-                              .setPageIndex(index: index);
-                        },
-                      ),
-                      items: carouselItemsModel.map((item) {
-                        return Builder(
-                          builder: (BuildContext context) {
-                            return GestureDetector(
-                              onTap: carouselOnTap,
-                              child: CarouselCard(
-                                imageUrl: item.imageUrl,
-                                text: item.title,
-                                location: item.locationTitle,
-                              ),
-                            );
-                          },
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  // Page Indicator
-                  Align(
-                    alignment: Alignment.center,
-                    child: BlocBuilder<PageIndicatorCubit, PageIndicatorState>(
-                      builder: (context, state) {
-                        if (state is PageIndicatorInitial) {
-                          return AnimatedSmoothIndicator(
-                            activeIndex: state.pageIndex,
-                            count: carouselItemsModel.length,
-                            effect: const ExpandingDotsEffect(
-                              activeDotColor: primaryColor,
-                              dotHeight: 9,
-                              dotWidth: 9,
-                              spacing: 6,
+                  (lisUpComingEvents.isNotEmpty)
+                      ? Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              top: Dimens.defaultPadding),
+                          child: CarouselSlider(
+                            carouselController: _carouselController,
+                            options: CarouselOptions(
+                              height: 200.0,
+                              autoPlay: true,
+                              autoPlayInterval: const Duration(seconds: 3),
+                              autoPlayAnimationDuration:
+                                  const Duration(milliseconds: 800),
+                              autoPlayCurve: Curves.easeInOut,
+                              enlargeCenterPage: true,
+                              onPageChanged: (index, reason) {
+                                context
+                                    .read<PageIndicatorCubit>()
+                                    .setPageIndex(index: index);
+                              },
                             ),
-                            onDotClicked: (index) {
-                              _carouselController.animateToPage(index);
+                            items: lisUpComingEvents.map((item) {
+                              return Builder(
+                                builder: (BuildContext context) {
+                                  return GestureDetector(
+                                    onTap: carouselOnTap,
+                                    child: CarouselCard(
+                                      imageUrl: item.imageUrls.first,
+                                      text: item.title,
+                                      location: item.location,
+                                    ),
+                                  );
+                                },
+                              );
+                            }).toList(),
+                          ),
+                        )
+                      : Container(),
+                  (lisUpComingEvents.isNotEmpty)
+                      ? const SizedBox(height: 16.0)
+                      : Container(),
+                  // Page Indicator
+                  (lisUpComingEvents.isNotEmpty)
+                      ? Align(
+                          alignment: Alignment.center,
+                          child: BlocBuilder<PageIndicatorCubit,
+                              PageIndicatorState>(
+                            builder: (context, state) {
+                              if (state is PageIndicatorInitial) {
+                                return AnimatedSmoothIndicator(
+                                  activeIndex: state.pageIndex,
+                                  count: lisUpComingEvents.length,
+                                  effect: const ExpandingDotsEffect(
+                                    activeDotColor: primaryColor,
+                                    dotHeight: 9,
+                                    dotWidth: 9,
+                                    spacing: 6,
+                                  ),
+                                  onDotClicked: (index) {
+                                    _carouselController.animateToPage(index);
+                                  },
+                                );
+                              } else {
+                                return Container();
+                              }
                             },
-                          );
-                        } else {
-                          return Container();
-                        }
-                      },
-                    ),
-                  ),
+                          ),
+                        )
+                      : Container(),
                   const TitleText(titleText: "Today’s Weather"),
                   // const WeatherUpdateWidget(),
                   const WeatherUpdateWidget(),
-                  const TitleText(titleText: "Populer destinations"),
-                  Padding(
-                    padding: const EdgeInsets.all(Dimens.defaultPadding),
-                    child: SizedBox(
-                      height: 200,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: destinations.length,
-                        itemBuilder: (context, index) {
-                          final destination = destinations[index];
-                          return PopularDestinationCard(
-                            title: destination.title,
-                            location: destination.locationTitle,
-                            imageUrl: destination.imageUrl,
-                            rating: destination.rating ?? 0.0,
-                          );
-                        },
-                      ),
-                    ),
-                  ),
+                  (lisRatedDetailModel.isNotEmpty)
+                      ? const TitleText(titleText: "Populer destinations")
+                      : Container(),
+                  (lisRatedDetailModel.isNotEmpty)
+                      ? Padding(
+                          padding: const EdgeInsets.all(Dimens.defaultPadding),
+                          child: SizedBox(
+                            height: 200,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: lisRatedDetailModel.length,
+                              itemBuilder: (context, index) {
+                                final destination = lisRatedDetailModel[index];
+                                return GestureDetector(
+                                  onTap: () {
+                                    context.toNamed(
+                                        ScreenRoutes.toItemDetailScreen,
+                                        args: destination);
+                                  },
+                                  child: PopularDestinationCard(
+                                    title: destination.title,
+                                    location: destination.location,
+                                    imageUrl: destination.imageUrls.first,
+                                    rating: destination.rating,
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        )
+                      : Container(),
                   const TitleText(titleText: "Discover Your Next Adventure"),
                   SizedBox(
                     height: 400,
